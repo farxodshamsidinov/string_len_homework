12e302db32d4f9a9c6e683fcc90baf72d83cb8eb
farxodshamsidinov <shamsidinovfarxod3@gmail.com>
2023-03-14 07:57:53 +0300

    07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,33 @@ def main(s1,s2,s3):
     Returns:
         string
     """
+    if len(s1)%2==0 and len(s2)==0 and len(s3)%2==0:
+
+        return "[]"
+    
+    elif len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1:
+        return f"[{s1, s2, s3}]"
+    
+    elif len(s1)%2==1 and len(s2)%2==1:
+        return f"[{s1, s2}]"
+    
+    elif len(s1)%2==1 and len(s3):
+        return f"[{s1, s3}]"
+    
+    elif len(s2)%2==1 and len(s3):
+        return f"[{s1, s2}]"
+    
+    elif len(s1)%2==1:
+        return f"[{s1}]"
+    
+    elif len(s2)%2==1:
+        return f"[{s2}]"
+    
+    elif len(s3)%2==1:
+        return f"[{s3}]" 
+
+
+
+
+
     return
